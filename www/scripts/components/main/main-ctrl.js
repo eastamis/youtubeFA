008b0854420d811ddbfaa4a8a6797e45605d9f9c
eastamis <eastamis@gmail.com>
2014-12-16 03:04:49 +0800

    add page indicator

--- www/scripts/components/main/main-ctrl.js
+++ www/scripts/components/main/main-ctrl.js
@@ -1,13 +1,10 @@
 'use strict';
 
 angular.module('youtube-client')
-  .controller('MainCtrl', function ($scope, $rootScope, $window, $famous, googleService, youtube) {
+  .controller('MainCtrl', function ($scope, $rootScope, $timeout, $window, $famous, googleService, youtube) {
     var Transitionable = $famous['famous/transitions/Transitionable'];
     var Easing = $famous['famous/transitions/Easing'];
     var EventHandler = $famous['famous/core/EventHandler'];
-
-    // famo.us scroller view handler
-    $scope.scrollHandler = new EventHandler();
 
     // buffer for storing video data
     var _buf = {
@@ -17,79 +14,46 @@ angular.module('youtube-client')
       currSelect: 'popular'
     };
 
-    // Am empty page in beginning
-    $scope.pages = [];
-
-    // set size of content area,
-    // IT"S IMPORTANT for scrollview pagination
-    $scope.contentArea = [$window.innerWidth, $window.innerHeight-120];
-
     // calculate the grid cells
     var CELL_SIZE = { width: 120, height: 90 },
       // content height needs to sub the height of header & footer
-      _rows = Math.floor($scope.contentArea[1] / CELL_SIZE.height),
-      _columns = Math.floor($scope.contentArea[0] / CELL_SIZE.width);
+      _contentHeight = $window.innerHeight - 120,
+      _contentWidth = $window.innerWidth,
+      _rows = Math.floor(_contentHeight / CELL_SIZE.height),
+      _columns = Math.floor(_contentWidth / CELL_SIZE.width);
+
     var _arrangePages = function() {
       var type = _buf.currSelect,
-        pageTotal = _columns * _rows;
-      for (var used=0, i=0, l=Math.ceil(_buf[type].length/pageTotal); i<l; i+=1) {
+        pageTotal = _columns * _rows,
+        pages = Math.ceil(_buf[type].length/pageTotal);
+      for (var used=0, i=0; i<pages; i+=1) {
         var num = Math.min(pageTotal, (_buf[type].length - used));
         $scope.pages.push({ videos: _buf[type].slice(used, num+used) });
         used += num;
       }
       // reset to 1st page
-      //var contentScrollView = $famous.find('#content-scroll-view')[0].renderNode;
-      //contentScrollView.goToPage(0);
+      //_contentScrollView.goToPage(0);
+      $scope.currentPage = 0;
+
+      // update position of page indicator
+      $scope.pageIndicAnchor = $scope.pageIndicItemWidth * pages * .5 * -1;
     };
 
-    // set famo.us viewer options
-    $scope.options = {
-      horiScrollView: {
-        direction: 0,
-        //pageStopSpeed: 10,
-        //pageSwitchSpeed: 0.5,
-        paginated: true
-      },
-      gridLayoutOptions: {
-        dimensions: [_columns, _rows]
-      }
-    };
-
-    //
-    // Prepare Google API ---- Begin
-    //
-    var _loginFlag = false;
-    var _loginWorkflow = function() {
-      googleService.login().then(function(){
-        _loginFlag = true;
-        $scope.loadVideos('subscript');
-      }, function(){
-        $scope.authTranslate.set([0, 0, 100], {duration: 1444, curve: 'linear'});
-        $scope.needAuth = true;
+    // monitor current page
+    var _contentScrollView = null;
+    $timeout(function() {
+      _contentScrollView = $famous.find('#content-scroll-view')[0].renderNode;
+      _contentScrollView.on('settle', function() {
+        $scope.currentPage = _contentScrollView.getCurrentIndex();
+        $scope.$apply('currentPage');
       });
-    };
+    }, 400);
 
-    // why $window.innerHeight*4, avoid it's appear after screen rotation
-    // TODO: hide the auth dialog...!
-    $scope.authTranslate = new Transitionable([0, $window.innerHeight*4, 100]);
-    $scope.needAuth = false; // set true if need to get google oauth token
-    $scope.onAuthClick = function() {
-      googleService.handleAuthClick().then(function(){
-        $scope.authTranslate.set([0, $window.innerHeight*4, 100], {duration: 444, curve: 'linear'});
-        $scope.needAuth = false;
-        _loginFlag = true;
-        $scope.loadVideos('subscript');
-      }, function(){
-        alert('Please accept to get your subscription youtube video!');
-      });
-    };
-    //
-    // Prepare Google API ---- End
-    //
-
-    $scope.loading = false;
-    $scope.loadVideos = function(type, param){
-      $scope.loading = true;
+    // query videos
+    var _loading = false;
+    var _loadVideos = function(type, param){
+      if (_loading) { return; } // preview task has not finished yet
+      _loading = true;
       var promise;
       switch (type) {
       case 'search':
@@ -104,7 +68,7 @@ angular.module('youtube-client')
         break;
       }
       promise.success(function(data){
-        $scope.loading = false;
+        _loading = false;
         _buf[type] = _.map(data.feed.entry, function(video){
           var scale = new Transitionable([.001, .001, .001]);
           var opacity = new Transitionable(0);
@@ -124,8 +88,68 @@ angular.module('youtube-client')
       })
     };
 
-    // show the most popular videos in beginning
-    $scope.loadVideos('popular', 'today');
+    //
+    // Prepare Google API ---- Begin
+    //
+    var _loginFlag = false;
+    var _loginWorkflow = function() {
+      googleService.login().then(function(){
+        _loginFlag = true;
+        _loadVideos('subscript');
+      }, function(){
+        $scope.authTranslate.set([0, 0, 100], {duration: 1444, curve: 'linear'});
+        $scope.needAuth = true;
+      });
+    };
+
+    // why $window.innerHeight*4, avoid it's appear after screen rotation
+    // TODO: hide the auth dialog...!
+    $scope.authTranslate = new Transitionable([0, $window.innerHeight*4, 100]);
+    $scope.needAuth = false; // set true if need to get google oauth token
+    $scope.onAuthClick = function() {
+      googleService.handleAuthClick().then(function(){
+        $scope.authTranslate.set([0, $window.innerHeight*4, 100], {duration: 444, curve: 'linear'});
+        $scope.needAuth = false;
+        _loginFlag = true;
+        _loadVideos('subscript');
+      }, function(){
+        alert('Please accept to get your subscription youtube video!');
+      });
+    };
+    //
+    // Prepare Google API ---- End
+    //
+
+    // famo.us scroller view handler
+    $scope.scrollHandler = new EventHandler();
+
+    // Am empty page in beginning
+    $scope.pages = [];
+
+    // set size of content area,
+    // IT"S IMPORTANT for scrollview pagination
+    $scope.contentArea = [_contentWidth, _contentHeight];
+
+    // position of the 1st element of page indicator
+    $scope.pageIndicAnchor = 0;
+    $scope.pageIndicItemWidth = 20; // item width of page indicator
+    $scope.currentPage = 0; // current page
+
+    // set famo.us viewer options
+    $scope.options = {
+      horiScrollView: {
+        direction: 0,
+        //pageStopSpeed: 10,
+        //pageSwitchSpeed: 0.5,
+        paginated: true
+      },
+      gridLayoutOptions: {
+        dimensions: [_columns, _rows]
+      },
+      pageIndicOptions: {
+        direction: 0 // vertical = 1 (default), horizontal = 0
+      }
+    };
 
     // animation while add new video
     $scope.animEnter = function(video, $done){
@@ -138,20 +162,20 @@ angular.module('youtube-client')
       term: $rootScope.searchTerm
     }
     $scope.updateSearch = function(){
-      if (!$scope.loading) {
-        $scope.loadVideos('search', $scope.search.term);
-        $rootScope.searchTerm = $scope.search.term;
-      }
+      _loadVideos('search', $scope.search.term);
+      $rootScope.searchTerm = $scope.search.term;
     }
     // search videos ---- End
 
+    //
     // tab selector ---- Begin
+    //
     var tabWidth = $window.innerWidth / 3;    // dimension
     var FOOTER_HEIGHT = 60;
     $scope.tabSelect = 0;    // current selection
     $scope.tabTitle = 'Recommand';
     // tab members
-    $scope.items = [
+    $scope.tabs = [
       { id: 'tab0', name: 'Popular', width: tabWidth, height: FOOTER_HEIGHT },
       { id: 'tab1', name: 'Subscription', width: tabWidth, height: FOOTER_HEIGHT },
       { id: 'tab2', name: 'Search', width: tabWidth, height: FOOTER_HEIGHT }
@@ -193,21 +217,39 @@ angular.module('youtube-client')
         }
         break;
       }
-    }
+    };
+    //
+    // tab selector ---- End
+    //
+
+    // page indicator --- Begin
+    $scope.onPageIndicClick = function(index) {
+      $scope.currentPage = index;
+      _contentScrollView.goToPage(index);
+    };
+    //
+    // page indicator --- End
+
     // monitor window resize event
     angular.element($window).bind('resize', function(){
-      var w = $window.innerWidth / 3;
-      for (var i=0, l=$scope.items.length; i<l; i+=1){
-        $scope.items[i].width = w;
+      var w = $window.innerWidth / $scope.tabs.length;
+      for (var i=0, l=$scope.tabs.length; i<l; i+=1){
+        $scope.tabs[i].width = w;
       }
       // adjust grid view
-      $scope.pages.length = 0;
-      _rows = Math.floor(($window.innerHeight-120) / CELL_SIZE.height),
-      _columns = Math.floor($window.innerWidth / CELL_SIZE.width),
+      _contentHeight = $window.innerHeight - 120;
+      _contentWidth = $window.innerWidth;
+      $scope.contentArea = [_contentWidth, _contentHeight];
+      _rows = Math.floor(_contentHeight / CELL_SIZE.height),
+      _columns = Math.floor(_contentWidth / CELL_SIZE.width),
       $scope.options.gridLayoutOptions.dimensions = [_columns, _rows];
+      // empty all grid items
+      $scope.pages.length = 0;
       _arrangePages();
-      //
-      $scope.$apply('items');
+      // apply change
+      $scope.$apply();
     });
-    // tab selector ---- End
+
+    // show the most popular videos in the beginning
+    _loadVideos('popular', 'today');
   });
